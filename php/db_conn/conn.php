40c90e94e90f193c9bb4838d00984899a973d72b
Leonardo Bianchi <leonardo.bianchi@davincifascetti.it>
2022-02-17 15:27:20 +0100

    general fixes

--- php/db_conn/conn.php
+++ php/db_conn/conn.php
@@ -1,7 +1,5 @@
 <?php
 
-$conn = null;
-
 function connect()
 {
 
--- php/func.php
+++ php/func.php
@@ -6,12 +6,37 @@ $creds = [
     "user3" => "pw3"
 ];
 
-function checkCreds($user, $pw)
+function oldCheckCreds($user, $pwd)
 {
     global $creds;
-    if (array_key_exists($user, $creds) && $creds[$user] == $pw) {
-        return TRUE;
+    if (array_key_exists($user, $creds) && $creds[$user] == $pwd) {
+        return true;
     } else {
-        return FALSE;
+        return false;
+    }
+}
+
+function checkCreds($user, $pwd)
+{
+    require_once("db_conn/conn.php");
+
+    $conn = connect();
+
+    if ($conn->connect_error) {
+        $errMsg = urlencode("Errore di sistema, riprovate più tardi.");
+        header("Location:errorPage.php?error=$errMsg&url_sent=../index.php&errno=1");
+    }
+
+    $res = $conn->query("SELECT user, pwd FROM creds WHERE user = $user AND pwd = $pwd");
+
+    if($res == false){
+        $errMsg = urlencode("Errore di sistema, riprovate più tardi.");
+        header("Location:errorPage.php?error=$errMsg&url_sent=../index.php&errno=2");
+    }else{
+        if($res->num_rows == 1){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
